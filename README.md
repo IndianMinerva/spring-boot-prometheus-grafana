52d97c6efc2e890cfe91a932ef4f0c73e96ce905
Arun <arun@Aruns-MacBook-Pro.local>
2022-10-12 03:15:29 +0200

    Adds Swagger

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ### Prometheus: http://localhost:9090
 #### Command to prometheus address in Grafana is: ifconfig | grep netmask | grep broadcast
 ### Application is at: http://localhost:8080
+### Swagger-ui: http://localhost:8080/swagger-ui.html
--- src/main/java/com/im/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplication.java
+++ src/main/java/com/im/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplication.java
@@ -2,21 +2,15 @@ package com.im.springbootprometheusgrafana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootPrometheusGrafanaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPrometheusGrafanaApplication.class, args);
     }
-
-    /*@Bean
-    public SecurityWebFilterChain securityWebFilterChain(
-            ServerHttpSecurity http) {
-        return http.authorizeExchange()
-                .pathMatchers("/actuator/**").permitAll()
-                .anyExchange().authenticated()
-                .and().build();
-    }*/
 }
 
